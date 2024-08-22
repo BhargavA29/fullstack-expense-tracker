6bbf9178c92df6fec2dda751b5790cb7864bcaa0
BhargavA29 <bhargavayare@gmail.com>
2024-08-22 05:55:11 +0530

    Fixed Bugs

--- expense-tracker/app/_components/Hero.jsx
+++ expense-tracker/app/_components/Hero.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 function Hero() {
     return (
@@ -8,11 +10,11 @@ function Hero() {
                 <div className="mx-auto max-w-xl text-center">
                     <h1 className="text-3xl font-extrabold sm:text-5xl">
                         Manage Your Expense
-                        <strong className="font-extrabold text-primary sm:block">Control Your Money </strong>
+                        <strong className="font-extrabold text-primary sm:block">Control Your Money</strong>
                     </h1>
 
                     <p className="mt-4 sm:text-xl/relaxed">
-                        Start creating your budget and save ton of Money
+                        Start creating your budget and save tons of money.
                     </p>
 
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
@@ -23,17 +25,32 @@ function Hero() {
                             Get Started
                         </a>
 
+                        {/* Show Dashboard button only if the user is signed in */}
+                        <SignedIn>
+                            <Link href={'/dashboard'}>
+                                <button className="block w-full rounded bg-gray-200 px-12 py-3 text-sm font-medium text-black shadow hover:bg-gray-300 focus:outline-none focus:ring active:bg-gray-400 sm:w-auto">
+                                    Dashboard
+                                </button>
+                            </Link>
+                        </SignedIn>
 
+                        {/* Optionally, handle the signed out state */}
+                        <SignedOut>
+                            
+                        </SignedOut>
                     </div>
                 </div>
             </div>
 
-            <Image src={'/dashboard.webp'} alt='dashboard'
+            <Image
+                src={'/dashboard.jpg'}
+                alt='dashboard'
                 width={1000}
-                height={700}
-                className='mt-9 rounded-xl border-2'></Image>
+                height={900}
+                className='rounded-xl border-2 mb-10'
+            />
         </section>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
